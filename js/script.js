22aad012cac9154a77d8c8985d9a3da902aa5e6d
chris houghton <cehoughton71@gmail.com>
2015-10-09 20:44:06 -0700

    Test out input function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $('#blanks form').submit(function(event) {
     var bookInput = $("input#book").val();
+    var movieInput = $("input#movie").val();
+    var colorInput = $("input#color").val();
+    var planetInput = $("input#planet").val();
 
+    $('ul').append(bookInput);
+    $('ul').append(movieInput);
+    $('ul').append(colorInput);
 
-    $('ul').append("bookInput");
-    
     $('#list').show();
     event.preventDefault();
   });
